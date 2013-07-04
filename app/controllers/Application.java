ef3cfe7a6a1e77269438ac9d578a27aee69a60cd
Philip Johnson <johnson@hawaii.edu>
2013-07-03 14:24:40 -1000

    Adding an unused import (to trigger a PMD warning) and an unused field (to trigger a Findbugs warning)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,14 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import java.util.List;  // To trigger a PMD warning.
 
 public class Application extends Controller {
-  
+    private Object uselessField = null;  // To trigger a FindBugs warning.
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-  
+    
+
 }
