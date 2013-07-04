8466537b2b63fb1fe870fb85b64b14627ec3733b
Philip Johnson <johnson@hawaii.edu>
2013-07-03 15:23:14 -1000

    Commenting out the PMD and FindBugs errors and removing some Checkstyle to make the QA trend more interesting.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,14 @@
 package controllers;
 
 import play.*;
-import play.mvc.*;
+import play.mvc.Result;
+import play.mvc.Controller;
+import static play.mvc.Results.ok;
 import views.html.*;
-import java.util.List;  // To trigger a PMD warning.
+//import java.util.List;  // To trigger a PMD warning.
 
 public class Application extends Controller {
-    private Object uselessField = null;  // To trigger a FindBugs warning.
+//    private Object uselessField = null;  // To trigger a FindBugs warning.
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
