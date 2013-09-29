0bf144d9bff58995c83a1f0c2d30a379ec09614f
Mike Slinn <mslinn@micronauticsresearch.com>
2013-09-29 11:58:41 -0700

    Cleaned up tests

--- test/tests/ApplicationTest.java
+++ test/tests/ApplicationTest.java
@@ -1,23 +1,14 @@
 package tests;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.codehaus.jackson.JsonNode;
 import org.junit.*;
-
 import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
 import play.i18n.Lang;
 import play.libs.F;
 import play.libs.F.*;
-
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
 
 
 /**
@@ -28,18 +19,18 @@ import static org.fest.assertions.Assertions.*;
 */
 public class ApplicationTest {
 
-    @Test 
+    @Test
     public void simpleCheck() {
         int a = 1 + 1;
         assertThat(a).isEqualTo(2);
     }
-    
+
     @Test
     public void renderTemplate() {
         Content html = views.html.index.render("Your new application is ready.");
         assertThat(contentType(html)).isEqualTo("text/html");
         assertThat(contentAsString(html)).contains("Your new application is ready.");
     }
-  
-   
+
+
 }
